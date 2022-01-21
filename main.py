ecc8e28938a2ecda439ebd6bc2f43d909876a029
Youth-Li <johnbrag@buffalo.edu>
2022-01-21 00:32:39 -0500

    does basic host discovery

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-import nmap
+from netaddr import IPAddress
 import os
 import socket
 import scapy
 import netifaces
+import nmap
 
 
 def getGate():
 	gate = netifaces.gateways()
 	return gate['default'][netifaces.AF_INET]
 
-gateway = getGate()
-print(gateway)
-interface = netifaces.ifaddresses(gateway[1])
-print(interface)
+def netScan():
+	map = nmap.PortScanner()
+	cidr =IPAddress(netprop['netmask']).netmask_bits()
+	map.scan(hosts=netprop['addr']+'/'+str(cidr) , arguments='-sn')
+
+	
+
+
+
+
+gateway = getGate()[0]
+interface = getGate()[1]
+
+#there is more info you can get from this like netmask addr broadcast just ask in [] when deref
+netprop = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]
+print(netprop['netmask'])
+netScan()
