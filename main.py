38a4d8abbc56bb59bf34f12159c736b428687bae
Youth-Li <johnbrag@buffalo.edu>
2022-01-21 01:05:38 -0500

    implemented looging in color, and getting host name, need to catch errors

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import socket
 import scapy
 import netifaces
 import nmap
-
+import coloredlogs, logging
 
 def getGate():
 	gate = netifaces.gateways()
@@ -14,11 +14,18 @@ def netScan():
 	map = nmap.PortScanner()
 	cidr =IPAddress(netprop['netmask']).netmask_bits()
 	map.scan(hosts=netprop['addr']+'/'+str(cidr) , arguments='-sn')
+	return map.all_hosts()		
 
-	
+def printHosts(hosts):
+	x = 0
+	print(hosts)
+	hostnames = {}
+	for host in hosts:
+		log.debug(socket.gethostbyaddr(host))
 
 
-
+log = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=log)
 
 gateway = getGate()[0]
 interface = getGate()[1]
@@ -26,4 +33,6 @@ interface = getGate()[1]
 #there is more info you can get from this like netmask addr broadcast just ask in [] when deref
 netprop = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]
 print(netprop['netmask'])
-netScan()
+netHosts = netScan()
+printHosts(netHosts)
+
